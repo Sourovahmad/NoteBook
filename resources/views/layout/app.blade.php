c5198bc03ab8a1ca0fef756c501dbcafb4c92f35
sourovahmad <mdsourovahmad19@gmail.com>
2020-07-20 15:28:20 +0600

    footer

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>CRUD APP</title>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/css/roboto.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/css/material-fullpalette.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/css/ripples.min.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+
+
+</head>
+
+<body>
+
+
+    <input type="text" value="{{route('home')}}" id="indexLink" hidden>
+
+    @include("layout.navbar")
+
+    <br>
+
+    @yield("content")
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/material.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/ripples.min.js"></script>
+
+    <script type="text/javascript">
+        $.material.init()
+    </script>
+
+
+
+
+
+
+<!-- ======= Footer ======= -->
+<footer id="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6 footer-info text-light">
+                    <h3>Abasas</h3>
+                    <p>We develop websites aiming for better user experience.
+                        Platform independence and mobile responsiveness are the key features of our product.
+                        We build your website from scratch, with your input and feedback at every step of the development process.</p>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="{{route('home')}}">Home</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">About us</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Services</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Terms of service</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Privacy policy</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <h4>Contact Us</h4>
+                    <p class="text-light">
+
+                        Head Office (USA)
+
+                        Address: 168-14 88 th ave Jamaica NY 11432
+                        Phone: +1 9293086108
+                        E-mail: usa@abasas.tech
+
+                    </p>
+
+                    <div class="social-links">
+                        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+                        <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+                        <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+                    </div>
+
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-newsletter">
+                    <h4>Our Newsletter</h4>
+                    <p class="text-light">Commenced operations in January, 2014 with a team of inspired developers, Abasas IT is a startup IT firm located at New York,USA. We are dedicated to helping organizations convert creative ideas into tangible value by providing reliable software solutions..</p>
+                    <form action="" method="post">
+                        <input type="email" name="email"><input type="submit" value="Subscribe">
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong>Abasas</strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            <!--
+        All the links in the footer should remain intact.
+        You can delete the links only if you purchased the pro version.
+        Licensing information: https://bootstrapmade.com/license/
+        Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=BizPage
+      -->
+            <h4 class="text-dark padding"> Designed by</h4> <a href="#">Sourov Ahmad (Abasas)</a>
+        </div>
+    </div>
+</footer>
+
+
+
+</body>
+
+</html>
